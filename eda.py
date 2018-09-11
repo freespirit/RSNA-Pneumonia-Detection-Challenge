88224798db786ee8b0b288e0dfa1c487cca0cef7
stan0 <stan.trifonov@gmail.com>
2018-09-11 09:19:20 +0300

    Very basic data exploration (e.g. distributions)

---
+++ eda.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+
+sns.set(style="white", palette="muted", color_codes=True)
+
+# %%
+# Read the data
+df_train_labels_orig = pd.read_csv("data/stage_1_train_labels.csv")
+
+# %%
+# Explore the data
+df_train_labels_orig.head()
+
+positive_samples = df_train_labels_orig[df_train_labels_orig['Target'] == 1]
+negative_samples = df_train_labels_orig[df_train_labels_orig['Target'] == 0]
+
+# %%
+counts = np.array([positive_samples.shape[0], negative_samples.shape[0]])
+labels = np.array(["Positive", "Negative"])
+plot = sns.barplot(x = labels, y = counts, saturation=.5)
+plot.set_title("Training Samples")
+
+# %%
+points = positive_samples[['x', 'y']]
+
+f, axes = plt.subplots(2, 2, figsize=(10, 10), sharey = 'row')
+axes[0, 0].set_title('X distribution')
+axes[0, 1].set_title('Y distribution')
+axes[1, 0].set_title('Bounding box distribution')
+axes[1, 1].set_title('Bounding box distribution')
+
+sns.distplot(points['x'], kde=False, color="b", ax=axes[0,0])
+sns.distplot(points['y'], kde=False, color="b", ax=axes[0,1])
+sns.scatterplot(x = "x", y = "y", data = points, ax=axes[1,0])
+sns.kdeplot(points['x'], points['y'], shade=True, ax=axes[1,1])
